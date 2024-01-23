8e44cb564a967f24685ee312e36691fbfb82b626
sverluss <156322401+sverluss@users.noreply.github.com>
2024-01-23 13:15:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function animateText() {
+        const textContainer = document.getElementById("text-container");
+        const words = textContainer.dataset.words.split(" ");
+        textContainer.innerHTML = ""; 
+
+        for (let i = 0; i < words.length; i++) {
+            const span = document.createElement("span");
+            span.textContent = words[i];
+            span.style.animationDelay = `${i * 0.5}s`; 
+            textContainer.appendChild(span);
+
+            if (i < words.length - 1) {
+                const space = document.createTextNode(" ");
+                textContainer.appendChild(space);
+            }
+        }
+    }
+    animateText();
+
+    setInterval(animateText, 5000);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopButton = document.querySelector(".scroll-to-top");
+
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+    scrollToTopButton.addEventListener("click", function() {
+        document.documentElement.scrollTop = 0; 
+    });
+});
+
+function showAlert() {
+    var confirmation = confirm("Вы уверены?");
+    if (confirmation) {
+        alert("Вы отправили запрос!");
+    } else {
+        alert("Вы не отправили запрос");
+    }
+}
